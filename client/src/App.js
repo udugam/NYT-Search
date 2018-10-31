7cd6127ec7c11a8b7a2c8f262beb42b78d83f3f1
udugam <ryan.udugam@gmail.com>
2018-10-31 13:30:46 -0400

    added reactstrap and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+//Reactstrap Bootstrap Components
+import {
+  Navbar,
+} from 'reactstrap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar color='light' light expand='md'>
+          <h1>New York Times Article Search</h1>
+        </Navbar>
       </div>
     );
   }
